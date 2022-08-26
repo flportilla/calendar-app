8a0c7f50bac12aa3d0cb35e9adad241f39ca8182
Fabian Portilla <fas_456@hotmail.com>
2022-08-26 16:14:32 -0500

    Fix non-serializable fake error

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { uiSlice, calendarSlice } from "./";
 
 export const store = configureStore({
+
+    //disable middleware to prevent false error 'non-serializable' value for a boolean
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+
     reducer: {
         ui: uiSlice.reducer,
         calendar: calendarSlice.reducer
