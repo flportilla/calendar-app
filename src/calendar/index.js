c83504a783430fbb137c8f478890415bb3fe9873
Fabian Portilla <fas_456@hotmail.com>
2022-08-26 17:08:20 -0500

    Createa button to add new Events

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,4 +1,5 @@
-export * from './pages/CalendarPage'
-export * from './components/Navbar'
 export * from './components/CalendarEventCell'
 export * from './components/CalendarModal'
+export * from './components/FabAddNew'
+export * from './components/Navbar'
+export * from './pages/CalendarPage'
