310a7520124072e659aafa6f273285bfb42a0084
Fabian Portilla <fas_456@hotmail.com>
2022-08-26 09:27:25 -0500

    Add login/register forms

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React from 'react';
+import './loginPage.css';
 
 export const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="password"
+                            />
+                        </div>
+                        <div className="d-grid mb-2">
+                            <input
+                                type="submit"
+                                className="btnSubmit"
+                                value="login"
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Register</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="name"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="password"
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Repeat password"
+                            />
+                        </div>
+
+                        <div className="d-grid gap-2">
+                            <input
+                                type="submit"
+                                className="btnSubmit"
+                                value="create account" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
